c6eb889e0b32a52c2c6987ec38847255fa955b3b
leosequani <156928045+leosequani@users.noreply.github.com>
2024-03-05 21:22:55 +0000

    finalsub

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 
 //include images into your bundle
@@ -6,21 +6,128 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	//State Hooks
 	const [todoItem, setTodoItem] = useState("")
 	const [listOfTodos, setTodoArr] = useState([])
+	const [bool, setbool] = useState(true)
+
+
+	//Global Variable
+	let arrTrsfr = []
+	let arrDel = []
+
+
+
+
+
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/Leo')
+			.then(response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				// Read the response as JSON
+				return response.json();
+			})
+			.then(responseAsJson => {
+				// Do stuff with the JSONified response
+				setTodoArr(responseAsJson);
+				console.log(responseAsJson);
+			})
+			.catch(error => {
+				console.log('Looks like there was a problem: \n', error);
+			});
+
+	}, [])
+
+
+
+
+
+
+
+
+
+
 
 	const createArrayTodo = (e) => {
 		if (e.key == "Enter") {
-			setTodoArr([...listOfTodos, todoItem]);
+			arrTrsfr = [...listOfTodos, {
+				"done": false,
+				"label": todoItem,
+			}];
 			setTodoItem("")
+			setTodoArr(arrTrsfr);
+			fetch('https://playground.4geeks.com/apis/fake/todos/user/Leo', {
+			method: 'PUT', // or 'POST'
+			body: JSON.stringify(arrTrsfr), // data can be a 'string' or an {object} which comes from somewhere further above in our application
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => console.log('Success:', response))
+			.catch(error => console.error(error))
 		}
+		
 	}
+
 	function removeElementAt(index) {
 		let arr = listOfTodos;
 		let frontPart = arr.slice(0, index);
 		let lastPart = arr.slice(index + 1); // index to end of array
+
+		arrDel = [...frontPart, ...lastPart]
 		setTodoArr([...frontPart, ...lastPart]);
+
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/Leo', {
+			method: 'PUT', // or 'POST'
+			body: JSON.stringify(arrDel), // data can be a 'string' or an {object} which comes from somewhere further above in our application
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => console.log('Success:', response))
+			.catch(error => console.error(error))
 	}
+
+	const checkBox = ((index) => {
+		
+		listOfTodos[index].done = !listOfTodos[index].done
+		setbool(!bool)
+
+		
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/Leo', {
+			method: 'PUT', // or 'POST'
+			body: JSON.stringify(listOfTodos), // data can be a 'string' or an {object} which comes from somewhere further above in our application
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => console.log('Success:', response))
+			.catch(error => console.error(error))
+
+			;
+
+
+	})
+	//{listOfTodos[index].done == true ? <input className="form-check-input" type="checkbox" value={listOfTodos[index].done} id="flexCheckChecked" checked />
+	//:<input className="form-check-input" type="checkbox" value={listOfTodos[index].done} id="flexCheckChecked" />}
+
 
 	return (
 		<div className="container-fluid">
@@ -31,9 +138,16 @@ const Home = () => {
 						{listOfTodos.map((todo, index) => {
 							return (
 								<div className="row" key={index}>
-									<div className="col-5"></div>
-									<li className="list-group-item" >
-										<i className="fas fa-trash me-2" onClick={(e) => removeElementAt(index)}></i>{todo}</li>
+									<li className="list-group-item d-flex justify-content-between " >
+										<div className="div">
+											<i className="fas fa-trash me-2" onClick={(e) => removeElementAt(index)}></i>
+											<span>{listOfTodos[index].label}</span>
+										</div>
+										<div>
+											<input className="form-check-input" type="checkbox" value={listOfTodos[index].done} id="flexCheckChecked" onChange={(e) => { checkBox(index) }} checked={todo.done == true ? true : false} />
+										</div>
+
+									</li>
 
 								</div>
 
@@ -47,3 +161,8 @@ const Home = () => {
 };
 
 export default Home;
+
+
+const name =()=>{
+	
+}
